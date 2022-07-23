2972a8d5805c797f19b90c5c50681f4c873dc5ca
am5g21 <am5g21@soton.ac.uk>
2022-07-22 23:25:46 -0400

    dded files

--- checksum.py
+++ checksum.py
@@ -6,7 +6,6 @@ def calcChecksum(data):
     checksum = bin(0)
     for i in range(0, int(len(data) / 32)):
         word = int(data[32 * i:32 * i + 32], 2)
-        print(word, "W")
         checksum = bin(int(checksum, 2) + word)
         if (len(str(checksum)) > 34):
             checksum = "0b" + checksum[3:]
@@ -24,7 +23,9 @@ def changeByte(byte, change):
 # changes input data until the desired checksum is reached
 def changeCheck(targetCheckSum, data):
     currentSum = calcChecksum(data)
-    difference = bin(targetCheckSum ^ int(currentSum, 16))
+    print(bin(targetCheckSum))
+    print(bin(int(currentSum, 2)))
+    difference = bin(targetCheckSum ^ int(currentSum, 2))
     count = 0
     while difference != bin(0):
         count += 1
@@ -34,27 +35,26 @@ def changeCheck(targetCheckSum, data):
 
         change = bin(2**currentBit)
 
-        random = int(ran.randint(0, int(len(data) / 32) * 32))
+        random = (int(ran.randint(0, int(len(data) / 32)))-1)*32
         word = data[random:random + 32]
         newWord = changeByte(word, change)
+        newWord = "0" * (34 - len(newWord)) + newWord[2:]
         data = data[0:random] + newWord + data[random + 32:]
         currentSum = calcChecksum(data)
-        newdiffrence = bin(targetCheckSum ^ int(currentSum, 2))
-        difference = newdiffrence
+        difference = bin(targetCheckSum ^ int(currentSum, 2))
+        print(bin(targetCheckSum), "T")
+        print(bin(int(currentSum, 2)))
     return data
 
 
-def createpacket(data, index, fileData):
-    index = (index)
-    checksum = index * (2**16) + int(data, 16)
+def createpacket(hiddenData, index, data):
+    checksum = index * (2**16) + int(hiddenData, 16)
     print(checksum, "2")
-    return changeCheck(checksum, fileData)
+    return changeCheck(checksum, data)
 
+def stringToBin(hex):
+    return bin(int(hex, 16))
 
 ran = random.Random()
 ran.seed(3)
-data = '10000000000000000000000000000001110000000000000000000000000000011000000000000000000000000000000111000000000000000000000000000001'
-print(calcChecksum(data), "1")
-newData = createpacket('1234', 1, data)
-print(newData, "3")
-print(calcChecksum(data), "4")
+d
